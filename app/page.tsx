04d23b3e0fd5be53a2e3cf225bd30ee796347569
Md. Abdul Kader <abdulemail789@gmail.com>
2024-06-12 19:55:07 +0600

    initialized nextjs project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import CategoryCard from "./components/categoryCard/CategoryCard";
+import ProductCard from "./components/productCard/ProductCard";
+
+export default function Home() {
+  return (
+    <main className="">
+      <CategoryCard/>
+      <ProductCard/>
+    </main>
+  );
+}
---
+++ app/shop/[productId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            Product details Page
+        </div>
+    );
+};
+
+export default page;
