be67733b9d09b62badd5682460da1c2af491d6d2
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-06-27 14:36:44 +0600

    add subscribe component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { ProductCard } from "@/components/common";
-import { Categories } from "@/components/home";
+import { Categories, Subscribe } from "@/components/home";
 
 export default function HomePage() {
   return (
     <main className="">
       <Categories />
       <ProductCard />
+      <Subscribe />
     </main>
   );
 }
