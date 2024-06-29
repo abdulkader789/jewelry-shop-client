48b7ecf237fdf8b0282d3f69e76fc75910972886
Md. Abdul Kader <abdulemail789@gmail.com>
2024-06-29 07:16:03 +0600

    merge with dev-abdulkader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
-import { MainLayout } from "@/components/layouts";
 import "@/styles/tailwind.css";
 import "@/styles/app.css";
 import { Footer, NavBar } from "@/components/common";
