b75608e24f571159b369135ba7b4aed2fc2ed97b
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-07-01 23:49:28 +0600

    fix overlay issue

--- src/components/home/AboutStore.tsx
+++ src/components/home/AboutStore.tsx
@@ -5,14 +5,14 @@ const AboutStore = () => {
     return (
         <div className="flex items-stretch lg:flex-row flex-col">
             <div className="lg:w-1/2 w-full p-10 relative flex flex-col bg-[url('/bg-about.jpg')] bg-cover bg-center">
-                <div className="absolute inset-0 bg-amber-200 opacity-55"></div>
+                <div className="absolute inset-0 bg-yellow-600 opacity-60"></div>
                 <div className="relative lg:left-40 left-0 z-10 lg:py-10 lg:pl-10 lg:pr-16 py-5 px-5 max-w-lg border-white border-solid border-4">
                     <h2 className="text-xl font-heading mb-1"><small>ABOUT</small></h2>
                     <h1 className="text-2xl font-heading mb-3">JEWELRY STORE</h1>
-                    <p className="mb-4 text-sm font-paragraph">
+                    <p className="mb-5 text-sm font-paragraph">
                         Jewelry is a prestigious global brand renowned for its exquisite and diverse collections. Celebrated by a vast community of loyal customers and Facebook fans, our stores around the world feature exclusive designs. We offer exceptional craftsmanship and occasional discounts, ensuring each piece is a timeless treasure.
                     </p>
-                    <button className="bg-white font-button px-4 py-2 rounded">Read More</button>
+                    <button className="bg-white text-black font-button px-4 py-2 rounded">Read More</button>
                 </div>
             </div>
 
